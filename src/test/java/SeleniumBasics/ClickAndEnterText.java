5a159f1f8bc83f70af0777152986b3d50c8884c2
NAVEEN <naveenchowdaryvemuri@gmail.com>
2023-06-12 11:19:45 +0530

    Switch to window(Window Handles)

--- src/test/java/SeleniumBasics/ClickAndEnterText.java
+++ src/test/java/SeleniumBasics/ClickAndEnterText.java
@@ -4,7 +4,14 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Set;
 
 public class ClickAndEnterText {
     @Test
@@ -15,6 +22,7 @@ public class ClickAndEnterText {
         WebDriver driver=new ChromeDriver();
         //To maximize web browser
         driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
         //Navigating to URL
         driver.get("https://www.letskodeit.com/practice");
         //clicking on webelement
@@ -27,9 +35,40 @@ public class ClickAndEnterText {
         txt_password.sendKeys("Naveen@143");
         WebElement btn_login=driver.findElement(By.xpath("//form[@name='loginform']//button[text()='Login']"));
         btn_login.click();
-
-        //to close all opened browsers
-
+        WebElement btn_Pratice=driver.findElement(By.xpath("(//a[text()='PRACTICE'])[1]"));
+        btn_Pratice.click();
+        String currentwindow=driver.getWindowHandle();
+        /**Switch to window by using title**/
+       /* Set<String> allwindows=driver.getWindowHandles();
+        for(String window:allwindows)
+        {
+            driver.switchTo().window(window);
+            String title=driver.getTitle();
+            if(title.equals("Practice Page"))
+            {
+                break;
+            }
+        }*/
+        /**switch to window by using arraylist**/
+        //ArrayList<String> obj=new ArrayList<>(2);
+        ArrayList<String> windows=new ArrayList<>(driver.getWindowHandles());
+        driver.switchTo().window(windows.get(1));
+        boolean displayed=driver.findElement(By.xpath("//legend[text()='Radio Button Example']")).isDisplayed();
+        Assert.assertTrue(displayed,"radio button example is not displaying");
+        WebDriverWait wait=new WebDriverWait(driver,Duration.ofSeconds(30));
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@id='benzradio']")));
+        WebElement radiobtn_benz=driver.findElement(By.xpath("//input[@id='benzradio']"));
+        radiobtn_benz.click();
+        boolean selected=radiobtn_benz.isSelected();
+        Assert.assertTrue(selected,"benz radio button is not selected");
+        WebElement radiobtn_bmw=driver.findElement(By.xpath("//input[@id='bmwradio']"));
+        boolean bmwselected=radiobtn_bmw.isSelected();
+        Assert.assertFalse(bmwselected,"bmw radio button not selected");
+        /***Switch to current window***/
+        //driver.switchTo().window(currentwindow);
+        /***close current window**/
+        driver.close();
+        /***to close all opened browsers***/
         driver.quit();
     }
 }
